73c7848e2bf51e0e1e75f4e16281adcd6f839afa
honmin123 <93572258+honmin123@users.noreply.github.com>
2021-12-20 17:26:59 +0800

    Update game.java

--- NKUST__C110181104/src/game.java
+++ NKUST__C110181104/src/game.java
@@ -1,5 +1,10 @@
 public class game {
     public static void main(String[] args) {
-
+       gameset Archer = new gameset("Archer",15,10,30);
+       Archer.get();
+       gameset Berserker = new gameset("Berserker",20,5,15);
+       Berserker.get();
+       gameset Magician = new gameset("Magician",10,30,40);
+       Magician.get();
     }
 }
