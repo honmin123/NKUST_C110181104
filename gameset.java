b756094d014256243858f5e7fc1265d5986f6473
Yulingsun <93572194+Yulingsun@users.noreply.github.com>
2021-12-20 16:10:30 +0800

    Add files via upload

---
+++ gameset.java
@@ -0,0 +1,12 @@
+public class gameset {
+    int hp;
+    int mp;
+    int atk;
+   String role;
+   gameset(String Role,int Hp,int Mp, int Atk){
+      role = Role;
+      hp = Hp;
+      mp = Mp;
+      atk = Atk;
+   }
+}
