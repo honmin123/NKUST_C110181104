0b93ed0c4bf0ec8cb961953dcc11f956826f1f65
flaz1031 <93572154+flaz1031@users.noreply.github.com>
2021-12-20 18:09:15 +0800

    Create game.java
    增加了攻擊 詠唱 跟穿裝備

--- NKUST__C110181104/src/game.java
+++ NKUST__C110181104/src/game.java
@@ -1,10 +1,27 @@
 public class game {
     public static void main(String[] args) {
+    System.out.println("Archer"+"遇到了Berserker和Magician");
+    System.out.println("Berserker"+"遇到了Archer和Magician");
+    System.out.println("Magician"+"遇到了Berserker和Archer");    
     gameset Archer = new gameset("Archer",15,10,30);
     Archer.get();
     gameset Berserker = new gameset("Berserker",20,5,15);
     Berserker.get();
     gameset Magician = new gameset("Magician",10,30,40);
     Magician.get();
+    System.out.println("----------------開始自動戰鬥-------------");
+    System.out.println("----------------第一回合-------------");
+    System.out.println("Archer"+"裝備了炸彈衣");
+    System.out.println("Berserker"+"裝備了炸彈衣");
+    System.out.println("Magician"+"裝備了炸彈衣");
+    System.out.println("----------------第二回合-------------");
+    System.out.println("Archer"+"發動自爆");
+    System.out.println("Berserker"+"發動自爆");
+    System.out.println("Magician"+"發動自爆");
+    System.out.println("Archer "+"Berserker "+"Magician "+":阿拉花瓜");
+    System.out.println("Archer"+"HP歸零");
+    System.out.println("Berserker"+"HP歸零");
+    System.out.println("Magician"+"HP歸零");
+    System.out.println("GAMEOVER");    
     }
 }
